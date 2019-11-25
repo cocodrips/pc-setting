f5e8dbfc2ffdc7d42b365e4fa68e421703fb7c5a
cocodrips <cocodrips@gmail.com>
2019-11-25 15:06:19 +0900

    Update

--- dotfiles/.zsh.d/inits/11_alias.sh
+++ dotfiles/.zsh.d/inits/11_alias.sh
@@ -8,10 +8,8 @@ alias cd="cdls"
 # delete branch
 alias clbranch="git branch | grep -v 'except' | grep -v '*' | xargs git branch -D"
 
-# peco
-function peco-src() {
-    peco | while read LINE; do $@ $LINE; done
-}
 
-alias d='ghq list --full-path | peco-src cd'
 
+alias -g L='| less'
+alias -g H='| head'
+alias -g G='| grep'
--- dotfiles/.zsh.d/inits/50_android.sh
+++ dotfiles/.zsh.d/inits/50_android.sh
@@ -1,4 +1,3 @@
-echo "android"
 export ANDROID_HOME=$HOME/Library/Android/sdk
 export ANDROID_NDK_HOME="$ANDROID_HOME/ndk/20.0.5594570"
 export PATH="$ANDROID_HOME/bin:$PATH"
